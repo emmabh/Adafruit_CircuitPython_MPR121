b40f10437289fe8fdc4660a6c4918d0dbc9d8dca
emmabh <emma@mschf.xyz>
2024-03-07 17:42:20 -0500

    smaller samples 2nd filter and bigger period

--- adafruit_mpr121.py
+++ adafruit_mpr121.py
@@ -243,7 +243,7 @@ class MPR121:
         self._write_register_byte(MPR121_DEBOUNCE, 0) # debouncing
         #self._write_register_byte(MPR121_CONFIG1, 0x20)  # default, 32xuA charge current
         self._write_register_byte(MPR121_CONFIG1, 0xBF)  # 18 samples, 63uA charge current
-        self._write_register_byte(MPR121_CONFIG2, 0x38)  # 0.5uS charge time, 18 samples, 1ms period between sample
+        self._write_register_byte(MPR121_CONFIG2, 0x24)  # 0.5uS charge time, 4 samples, 16ms period between sample
 
         # Enable all electrodes.
         self._write_register_byte(
