94863d86b1509db7ea7451e41ca17d949d743d94
emmabh <emma@mschf.xyz>
2024-03-07 17:47:34 -0500

    lower samples first filter

--- adafruit_mpr121.py
+++ adafruit_mpr121.py
@@ -242,7 +242,7 @@ class MPR121:
         # Set other configuration registers.
         self._write_register_byte(MPR121_DEBOUNCE, 0) # debouncing
         #self._write_register_byte(MPR121_CONFIG1, 0x20)  # default, 32xuA charge current
-        self._write_register_byte(MPR121_CONFIG1, 0xBF)  # 18 samples, 63uA charge current
+        self._write_register_byte(MPR121_CONFIG1, 0x3F)  # 6 samples, 63uA charge current
         self._write_register_byte(MPR121_CONFIG2, 0x24)  # 0.5uS charge time, 4 samples, 16ms period between sample
 
         # Enable all electrodes.
